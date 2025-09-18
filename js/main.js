2df2a4d7a0c26bf587bf6f798a953492d88c4fdc
jiyuuun24 <leey5170@gmail.com>
2025-09-18 15:25:35 +0900

    ver1.5

--- js/main.js
+++ js/main.js
@@ -293,7 +293,7 @@ var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
+  this.period = parseInt(period, 10) || 3000;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
@@ -312,7 +312,7 @@ TxtRotate.prototype.tick = function() {
   this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
   var that = this;
-  var delta = 300 - Math.random() * 100;
+  var delta = 200 - Math.random() * 100;
 
   if (this.isDeleting) { delta /= 2; }
 
